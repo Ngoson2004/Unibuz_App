ea30d271832214030626dcb119c6dab7ff264e98
Simon Nguyen <80842534+cobeo2004@users.noreply.github.com>
2025-02-24 01:28:48 +0700

    fix: add setOpenSearch

--- src/shared/context/AppContext.tsx
+++ src/shared/context/AppContext.tsx
@@ -3,18 +3,31 @@ import React, { createContext, useContext, useState } from "react";
 interface AppContextProps {
   openModal: boolean;
   setOpenModal: React.Dispatch<React.SetStateAction<boolean>>;
+  openSearch: boolean;
+  setOpenSearch: React.Dispatch<React.SetStateAction<boolean>>;
   openDrawer: boolean;
   setOpenDrawer: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const AppContext = createContext<AppContextProps | undefined>(undefined);
 
-export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+export const AppProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const [openModal, setOpenModal] = useState(false);
   const [openDrawer, setOpenDrawer] = useState(false);
-
+  const [openSearch, setOpenSearch] = useState(false);
   return (
-    <AppContext.Provider value={{ openModal, setOpenModal, openDrawer, setOpenDrawer }}>
+    <AppContext.Provider
+      value={{
+        openModal,
+        setOpenModal,
+        openDrawer,
+        setOpenDrawer,
+        openSearch,
+        setOpenSearch,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
