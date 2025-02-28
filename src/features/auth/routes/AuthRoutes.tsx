5b7deb96a8a2d0c8221fb72a115cf45254bea764
Simon Nguyen <80842534+cobeo2004@users.noreply.github.com>
2025-02-28 20:53:21 +1100

    chore: perform code spliting

--- src/features/auth/routes/AuthRoutes.tsx
+++ src/features/auth/routes/AuthRoutes.tsx
@@ -1,19 +1,24 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import SignInPage from "../pages/SignInPage";
-import SignUpPage from "../pages/SignUpPage";
-import AuthCode from "../pages/AuthCode";
-import Password from "../pages/Password";
 import AuthLayout from "../layouts/AuthLayout";
+import SuspenseLoadingPage from "@/shared/page/SuspenseLoadingPage";
+
+const SignInPage = React.lazy(() => import("../pages/SignInPage"));
+const SignUpPage = React.lazy(() => import("../pages/SignUpPage"));
+const AuthCode = React.lazy(() => import("../pages/AuthCode"));
+const Password = React.lazy(() => import("../pages/Password"));
 
 export default function AuthRoutes() {
   return (
-    <Routes>
-      <Route element={<AuthLayout />}>
-        <Route path="sign-in" element={<SignInPage />} />
-        <Route path="sign-up" element={<SignUpPage />} />
-        <Route path="verify" element={<AuthCode />} />
-        <Route path="password" element={<Password />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<SuspenseLoadingPage />}>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="sign-in" element={<SignInPage />} />
+          <Route path="sign-up" element={<SignUpPage />} />
+          <Route path="verify" element={<AuthCode />} />
+          <Route path="password" element={<Password />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
--- src/features/home/message/routes/MessageRoutes.tsx
+++ src/features/home/message/routes/MessageRoutes.tsx
@@ -1,10 +1,15 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import MessagePage from "../pages/MessagePage";
+import SuspenseLoadingPage from "@/shared/page/SuspenseLoadingPage";
+
+const MessagePage = React.lazy(() => import("../pages/MessagePage"));
 
 export default function MessageRoutes() {
   return (
-    <Routes>
-      <Route index element={<MessagePage />} />
-    </Routes>
+    <Suspense fallback={<SuspenseLoadingPage />}>
+      <Routes>
+        <Route index element={<MessagePage />} />
+      </Routes>
+    </Suspense>
   );
 }
